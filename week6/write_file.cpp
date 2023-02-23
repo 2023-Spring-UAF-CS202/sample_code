0230b9ab44cfd0917dbaf1cb08a43387b6c5dd4f
Lane Schwartz <dowobeha@gmail.com>
2023-02-23 11:19:34 -0900

    Thursday class examples

--- week6/write_file.cpp
+++ week6/write_file.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 
+using std::cerr;
 using std::cout;
 using std::endl;
 using std::string;
@@ -16,9 +17,24 @@ int main(int argc, char * * argv) {
   string program_name{argv[0]};
   vector<string> args{&argv[1], &argv[argc]};
 
-  if (args.size() > 1) {
+  if (args.size() != 2) {
+
+    cerr << "Usage: " << program_name << " input_file output_file" << endl;
+    
+  } else {
+    
     ifstream in{args[0]};
+
+    if (in.fail()) {
+      cerr << "Error opening input file " << args[0] << endl;
+      return -1;
+    }
+
     ofstream out{args[1]};
+    if (out.fail()) {
+      cerr << "Error opening output file " << args[1] << endl;
+      return -2;
+    }
     
     string token;
     while (in >> token and out) {
