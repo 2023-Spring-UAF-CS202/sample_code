8ccaf6ade5acd412bcf342aa2ddcf5c35f18cba1
Lane Schwartz <dowobeha@gmail.com>
2023-02-28 11:03:25 -0900

    Don't use ++

---
+++ week7/dumb_operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::endl;
+using std::cout;
+
+
+int main() {
+
+  int c{0};
+
+  cout << c++ << endl; // Prints 0
+
+  cout << ++c << endl; // Prints 2
+
+  //  cout << c+=1 << endl; // Gives a syntax error
+  
+}
