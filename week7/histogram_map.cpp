2d9581cb88a905024d1f691fd837c10dc8c4fd82
Lane Schwartz <dowobeha@gmail.com>
2023-02-28 10:15:06 -0900

    Initial code with map

--- week7/histogram_map.cpp
+++ week7/histogram_map.cpp
@@ -1,7 +1,24 @@
 #include <map>
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::map;
 
 
 int main() {
+
+  map<string, int> hw01_grades {
+				  {"A", 8},
+				  {"B", 5},
+				  {"C", 0},
+				  {"D", 3},
+				  {"F", 5}
+  };
+
+  cout << "There are " << hw01_grades.size() << " elements " << endl;
 
   
   return 0;
