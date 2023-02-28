7974cfa175a845e93c33a7df8a8632335607552e
Lane Schwartz <dowobeha@gmail.com>
2023-02-28 11:17:19 -0900

    Example code for formatting

--- week7/formatting.cpp
+++ week7/formatting.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <iomanip>  // Provides setw
+
+using std::setw;
 
 using std::cout;
 using std::endl;
 
 using std::boolalpha;
 using std::noboolalpha;
+
+using std::fixed;
+using std::scientific;
 
 int main() {
 
@@ -18,6 +24,14 @@ int main() {
 
   cout << false << endl;
   cout << true << endl;
+
+  cout << scientific;
+  cout << 100000318000000.0 << endl;
+
+  cout << std::fixed;
+  cout << std::setprecision(2);
+
+  cout << 3.141595 << endl;
   
   return 0;
 }
