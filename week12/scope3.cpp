e59f45033ec7139df0b61d1040d9831976623daa
Lane Schwartz <dowobeha@gmail.com>
2023-04-06 11:08:27 -0800

    Pass by value with object

---
+++ week12/scope3.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using std::cerr;
+using std::cout;
+using std::endl;
+
+int x{17}; // This is a global variable
+           // It is in scope throughout the entire program
+
+class Demo {
+public:
+
+  Demo(int v) : value{v}
+  {
+    cerr << "Running Demo(" << value << ")" << endl;
+  }
+
+  // destructor
+  ~Demo() {
+    cerr << "Running destructor ~Demo() for Demo object with value " << value << endl;
+  }
+  
+  int getValue() const {
+    return value;  // value *is* in scope
+  }
+  
+private:
+  int value;  // in scope anywhere in this class
+};
+
+int cubed(Demo d) {
+  int i = d.getValue();
+  return i*i*i;
+}
+
+
+int main() {
+
+  cout << x << endl;; // x is in scope
+  
+  // cout << y; // This is an error because y is not yet in scope
+  
+  Demo y{42}; // y is now in scope
+             //   from here to the end of the curly braces
+
+  // passing by value
+  cout << cubed(y) << endl;  // This is fine. y is now in scope
+
+  return 0;
+}
