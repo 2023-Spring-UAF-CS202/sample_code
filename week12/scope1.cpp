91fd48668b74018055a0fe48c4df4f2e51cea800
Lane Schwartz <dowobeha@gmail.com>
2023-04-06 10:14:37 -0800

    Pass by value

---
+++ week12/scope1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int x{17}; // This is a global variable
+           // It is in scope throughout the entire program
+
+int cubed(int i) {
+  return i*i*i;
+}
+
+
+int main() {
+
+  cout << x << endl;; // x is in scope
+  
+  // cout << y; // This is an error because y is not yet in scope
+  
+  int y{42}; // y is now in scope
+             //   from here to the end of the curly braces
+
+  // passing by value
+  cout << cubed(y) << endl;  // This is fine. y is now in scope
+
+  
+  
+  return 0;
+}
